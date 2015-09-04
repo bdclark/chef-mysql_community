c3ed899becc0df7aa1a85eb8f76524d5a295fc06
Brian Clark <brian@clark.zone>
2015-09-04 08:51:54 -0400

    bump ver

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'brian@clark.zone'
 license          'apache2'
 description      'Installs/Configures MySQL'
 long_description 'Installs/Configures MySQL'
-version          '0.1.0'
+version          '0.1.1'
 
 depends 'yum-mysql-community', '~> 0.1'
 depends 'mysqld', '~> 1.0.3'
